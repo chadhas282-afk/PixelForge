a99e05769abbdb1dcb150d2e06495a44035adc5a
chadhas282-afk <chadhas282@gmail.com>
2026-03-07 17:22:50 +0530

    feat: add event listeners for filter options to update active state and filter name

--- script.js
+++ script.js
@@ -146,3 +146,18 @@ const updateFilters = () => {
         }
     }
 };
+
+filterOptions.forEach(btn => {
+    btn.onclick = () => {
+        document.querySelector(".active").classList.remove("active");
+        btn.classList.add("active");
+        filterName.innerText = btn.innerText;
+        // const isSpecial = ["resize", "crop"].includes(btn.id);
+        // sliderContainer.style.display = isSpecial ? "none" : "block";
+        // resizePanel.style.display = btn.id === "resize" ? "block" : "none";
+        // cropControls.style.display = btn.id === "crop" ? "block" : "none";
+        // cropBox.style.display = btn.id === "crop" ? "block" : "none";
+        // isCropping = btn.id === "crop";
+        // updateFilters();
+    };
+});
