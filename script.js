21ffd2d55db48f3df26c9eed0816b5bdf743b8c0
chadhas282-afk <chadhas282@gmail.com>
2026-03-07 17:20:05 +0530

    add: implement applyState function to restore image processing state from saved data

--- script.js
+++ script.js
@@ -60,3 +60,11 @@ redoBtn.onclick = () => {
     historyStack.push(captureState());
     applyState(JSON.parse(redoStack.pop()));
 };
+
+const applyState = (s) => {
+    previewImg.src = s.src;
+    ({brightness, saturation, contrast, hue, blur, sepia, grayscale, inversion} = s.f);
+    ({rotate, flipH, flipV} = s.t);
+    widthInput.value = s.d.w; heightInput.value = s.d.h;
+    updateFilters();
+};
